cdb3178bb45f2980fda9b5cd8b746d7f36c75926
neil patel <neil@myindianweddingapp.com>
2015-11-21 21:09:12 +0000

    Created comments model , controller & views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+
+
+  def create 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    flash[:notice] = "Comment has been created!"
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    flash[:notice] = "Comment has been destroyed!"
+
+    redirect_to post_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :body)
+  end
+
+  def find_comments
+  end
+
+
+end
